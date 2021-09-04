7c3a35b307a104f427324be6f392904ccfa7fb28
Jeffrey Purvis <purvisjd@gmail.com>
2021-09-04 15:39:25 -0400

    updated through 11.5.3

--- static/js/app.js
+++ static/js/app.js
@@ -18,3 +18,25 @@ function buildTable(data) {
         });
     });
 }
+
+function handleClick(){
+    //Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    //check to see if a date was entered and filter data using that date
+    if (date) {
+        //apply filter to the table data to only keep rows where datetime value matches filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    //rebuild the table using the filtered data
+    //if no date was entered, then filteredData will just be original tableData
+    buildTable(filteredData);
+};
+
+//attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+??build the table when the page loads
+buildTable(tableData);
